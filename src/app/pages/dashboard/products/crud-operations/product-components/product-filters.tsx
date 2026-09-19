c109e90f2bd97d77336e0ee351165da7156d1e8a
kavikarthik-interloid <kavikarthik@interloid.com>
2026-09-19 18:28:03 +0530

    remove the reset column button

--- src/app/pages/dashboard/products/crud-operations/product-components/product-filters.tsx
+++ src/app/pages/dashboard/products/crud-operations/product-components/product-filters.tsx
@@ -315,7 +315,6 @@ export function ProductFilters({
 
           {renderPriceSelect()}
           {renderDensityDropdown()}
-          {renderResetColumnsButton()}
           {renderResetButton()}
         </div>
 
